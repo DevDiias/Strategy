2d96371e8954976cc28ab132d2253660760f7dd6
Isabelly Santos <isabelly.santos885@gmail.com>
2023-04-25 21:37:16 -0300

    Design Patterns - Strategy

---
+++ MediaDisciplina/src/Disciplina.java
@@ -0,0 +1,45 @@
+public class Disciplina {
+    private ICalcMedia CalcMedia;
+    private String nome;
+    private double p1;
+    private double p2;
+    public double media;
+    public String situacao;
+
+    public Disciplina(ICalcMedia calcMedia) {
+        this.CalcMedia = calcMedia;
+    }
+
+    public void setNome(String name) {
+        this.nome = name;
+    }
+
+    public void setP1(double p1) {
+        this.p1 = p1;
+    }
+
+    public double getP1() {
+        return p1;
+    }
+
+    public void setP2(double p2) {
+        this.p2 = p2;
+    }
+
+    public double getP2() {
+        return p2;
+    }
+
+    public void CalcularMedia() {
+        media = CalcMedia.CalculaMedia(p1, p2);
+        situacao = CalcMedia.Situacao(media);
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+}
